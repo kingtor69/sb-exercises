8a9206e2a081462f814bb3d318f0a28668c4d1a6
Tor Kingdon <tor@hearkitty.com>
2021-04-17 15:37:40 -0600

    got through step 5 without following directions for step 3

--- 19_3_10-flask-tools-exercise/app.py
+++ 19_3_10-flask-tools-exercise/app.py
@@ -1,23 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
+from helpers import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "big-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+# initialize array for survey respones
 responses = []
+text_responses = []
+
+# this is where variable survey is currently hard-wired to the satisfaction_survey
+# may change in future development
+survey = satisfaction_survey
 
 @app.route('/')
-def show_satisfaction_survey():
-	num_questions = len(satisfaction_survey.questions)
-	any_text_allowed = False
-	columns = 2
-	for i in range(num_questions):
-		if satisfaction_survey.questions[i].allow_text:
-			any_text_allowed = True
-	if any_text_allowed:
-		columns = 3
+def landing_page():
+	"""for the time being, this redirects to the satisfaction_survey
+	future development may include options"""
+	# reinitialize global variables
+	responses = []
+	text_responses = []
+	survey = satisfaction_survey
 
-	return render_template('survey.html', survey = satisfaction_survey, num_questions = num_questions, columns = columns)
+	return redirect('/survey')
+
+@app.route('/survey')
+def show_survey():
+	"""create an HTML form to display the selected survey"""
+	(num_questions, columns) = survey_size(survey)
+
+	return render_template('survey.html', survey = survey, num_questions = num_questions, columns = columns)
+
+@app.route('/response', methods=["POST"])
+def process_response():
+# 	"""process user's responses to the survey and output to HTML template"""
+	(num_questions, columns) = survey_size(survey)
+	for i in range(num_questions):
+		responses.append(request.form[f'q{i}'])
+		text_responses.append(request.form[f't{i}'])
+
+	return render_template('response.html', survey = survey, responses = responses, text_responses = text_responses, num_questions = num_questions, columns = columns)
