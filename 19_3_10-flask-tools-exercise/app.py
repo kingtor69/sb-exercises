3882ed3e88cc90faf30a36739a9d7255ec17ff31
Tor Kingdon <tor@hearkitty.com>
2021-04-17 00:22:59 -0600

    19.3.10 survey page loading, nothing happens after that

--- 19_3_10-flask-tools-exercise/app.py
+++ 19_3_10-flask-tools-exercise/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,3 +7,17 @@ app.config['SECRET_KEY'] = "big-secret"
 
 debug = DebugToolbarExtension(app)
 
+responses = []
+
+@app.route('/')
+def show_satisfaction_survey():
+	num_questions = len(satisfaction_survey.questions)
+	any_text_allowed = False
+	columns = 2
+	for i in range(num_questions):
+		if satisfaction_survey.questions[i].allow_text:
+			any_text_allowed = True
+	if any_text_allowed:
+		columns = 3
+
+	return render_template('survey.html', survey = satisfaction_survey, num_questions = num_questions, columns = columns)
