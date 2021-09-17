5dd610096d5d68a8996b41e51093db8254618814
Tor Kingdon <tor@hearkitty.com>
2021-09-17 00:28:43 -0600

    32.2.10 done

--- 32_2-ExpressShoppingList/itemsRoutes.js
+++ 32_2-ExpressShoppingList/itemsRoutes.js
@@ -1,47 +1,58 @@
 const express = require("express")
 const router = new express.Router()
 const ExpressError = require("./expressError")
-const cats = require("./fakeDb")
+const items = require("./fakeDb")
 
 router.get("/", (req,res) => {
   return res.json({items});
 });
 
-router.get("/:name", function (req, res) {
-    const foundItem = items.find(item => item.name === req.params.name);
-    if(foundItem === undefined){
-      throw new ExpressError("Item not found", 404)
+router.get("/:name", function (req, res, next) {
+    try {
+        const foundItem = items.find(item => item.name === req.params.name);
+        if(foundItem === undefined){
+            throw new ExpressError("Item not found", 404)
+        };
+        res.json({ item: foundItem });
+    } catch(e) {
+        next(e);
     };
-    res.json({ item: foundItem });
   });
   
-  router.post("/", function (req, res) {
+  router.post("/", function (req, res, next) {
   try {
-    if(!req.body.name) throw new ExpressError("Name is required", 400);
-    const newCat = { name: req.body.name }
-    cats.push(newCat)
-    return res.status(201).json({ cat: newCat })
+    if (!req.body.name || !req.body.price) throw new ExpressError("Item must have a name and a price", 400);
+    const newItem = { name: req.body.name, price: req.body.price }
+    items.push(newItem)
+    return res.status(201).json({ added: newItem })
   } catch(e) {
     return next(e)
   }
 })
 
-router.patch("/:name", function (req, res) {
-  const foundCat = cats.find(cat => cat.name === req.params.name)
-  if (foundCat === undefined) {
-    throw new ExpressError("Cat not found", 404)
-  }
-  foundCat.name = req.body.name 
-  res.json({ cat: foundCat })
-})
+router.patch("/:name", function (req, res, next) {
+    try {
+      const foundItem = items.find(item => item.name === req.params.name)
+      if (foundItem === undefined) {
+          throw new ExpressError("Item not found", 404)
+        }
+        if (req.body.name) foundItem.name = req.body.name;
+        if (req.body.price) foundItem.price = req.body.price;
+        return res.json({ updated: foundItem });
+    } catch (e) {
+        next(e);
+    };
+});
 
-router.delete("/:name", function (req, res) {
-  const foundCat = cats.findIndex(cat => cat.name === req.params.name)
-  if (foundCat === -1) {
-    throw new ExpressError("Cat not found", 404)
-  }
-  cats.splice(foundCat, 1)
-  res.json({ message: "Deleted" })
-})
+router.delete("/:name", function (req, res, next) {
+    try {
+        const foundItem = items.findIndex(item => item.name === req.params.name)
+        if (foundItem === -1) throw new ExpressError("Item not found", 404)
+        items.splice(foundItem, 1)
+        return res.json({ message: "Deleted" })
+    } catch (e) {
+        next(e);
+    };
+});
 
 module.exports = router;
--- 32_2-ExpressShoppingList/itemsRoutes.test.js
+++ 32_2-ExpressShoppingList/itemsRoutes.test.js
@@ -46,58 +46,58 @@ describe("GET /items", () => {
     });
 });
 
-// describe("POST /items", () => {
-//     test("Adding an item to list", async() => {
-//         const resp = await request(app).post(`${apiPrefix}`).send({ name: "pudding",
-//             price: 1.99 });
-//         expect(resp.statusCode).toBe(201);
-//         expect(resp.body).toEqual({ 
-//             added: {
-//                 name: "pudding",
-//                 price: 1.99
-//             }
-//         });
-//     });
-//     test("Adding an item with invalid data", async () => {
-//         const resp = await request(app).post(`${apiPrefix}`).send({ 
-//             nom: "frommage_bleu",
-//             prix: 10 
-//         });
-//         expect(resp.statusCode).toBe(400);
-//     });
-// });
+describe("POST /items", () => {
+    test("Adding an item to list", async() => {
+        const resp = await request(app).post(`${apiPrefix}`).send({ name: "pudding",
+            price: 1.99 });
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body).toEqual({ 
+            added: {
+                name: "pudding",
+                price: 1.99
+            }
+        });
+    });
+    test("Adding an item with invalid data", async () => {
+        const resp = await request(app).post(`${apiPrefix}`).send({ 
+            nom: "frommage_bleu",
+            prix: 10 
+        });
+        expect(resp.statusCode).toBe(400);
+    });
+});
 
-// describe("PATCH /items/:name", () => {
-//     test("Updating an item's price", async() => {
-//         const resp = await request(app).patch(`${apiPrefix}/${list[0].name}`).send({ price: 0.95});
-//         expect(resp.statusCode).toBe(200);
-//         expect(resp.body).toEqual({ updated: { 
-//             name: list[0].name,
-//             price: 0.95 } 
-//         });
-//     });
-//     test("Changing an item's name", async() => {
-//         const resp = await request(app).patch(`${apiPrefix}/${list[0].name}`).send({ name: "dill_pickles"});
-//         expect(resp.statusCode).toBe(200);
-//         expect(resp.body).toEqual({ updated: { 
-//             name: "dill_pickles",
-//             price: list[0].price } 
-//         });
-//     });
-//     test("Trying to change an invalid item returns 404", async() => {
-//         const resp = await request(app).patch(`${apiPrefix}/buffalo_chips`).send({ price: 0.10 });
-//         expect(resp.statusCode).toBe(404);
-//     });
-// });
+describe("PATCH /items/:name", () => {
+    test("Updating an item's price", async() => {
+        const resp = await request(app).patch(`${apiPrefix}/${list[0].name}`).send({ price: 0.95});
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({ updated: { 
+            name: list[0].name,
+            price: 0.95 } 
+        });
+    });
+    test("Changing an item's name", async() => {
+        const resp = await request(app).patch(`${apiPrefix}/${list[0].name}`).send({ name: "dill_pickles"});
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({ updated: { 
+            name: "dill_pickles",
+            price: list[0].price } 
+        });
+    });
+    test("Trying to change an invalid item returns 404", async() => {
+        const resp = await request(app).patch(`${apiPrefix}/buffalo_chips`).send({ price: 0.10 });
+        expect(resp.statusCode).toBe(404);
+    });
+});
 
-// describe("DELETE /items/:name", () => {
-//     test("Deleting an item from list", async() => { 
-//         const resp = await request(app).delete(`${apiPrefix}/${list[0].name}`);
-//         expect(resp.statusCode).toBe(200);
-//         expect(resp.body).toEqual({ message: 'Deleted' });
-//     });
-//     test("Attempt to delete a non-existent cat returns 404", async() => {
-//         const resp = await request(app).delete(`${apiPrefix}/Piggles`);
-//         expect(resp.statusCode).toBe(404);
-//     });
-// });
+describe("DELETE /items/:name", () => {
+    test("Deleting an item from list", async() => { 
+        const resp = await request(app).delete(`${apiPrefix}/${list[0].name}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({ message: 'Deleted' });
+    });
+    test("Attempt to delete a non-existent item returns 404", async() => {
+        const resp = await request(app).delete(`${apiPrefix}/Piggles`);
+        expect(resp.statusCode).toBe(404);
+    });
+});
